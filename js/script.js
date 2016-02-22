2a72189803d8151bf9620528db4fd2cba3dba610
Abhinav Garg <abhinav.garg@live.com>
2016-02-22 17:02:44 +0530

    Adding running pane and some content changes as per semester change

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
 		            //         return this.value; // clean, unformatted number for year
 		            //     }
 		            // }
-		            categories: ['1', '2', '3', '4', '5', '6']
+		            categories: ['1', '2', '3', '4', '5', '6', '7']
 		        },
 		        yAxis: {
 		        	min: 8,
@@ -89,10 +89,10 @@ $(document).ready(function() {
 		        },
 		        series: [{
 		            name: 'SPI',
-		            data: [9.39, 8.94, 8.68, 9.58, 8.56, 9.39]
+		            data: [9.39, 8.94, 8.68, 9.58, 8.56, 9.39, 9.5]
 		        }, {
 		            name: 'CPI',
-		            data: [9.39, 9.16, 8.97, 9.1, 9.0, 9.05]
+		            data: [9.39, 9.16, 8.97, 9.1, 9.0, 9.05, 9.1]
 		        }]
 		    });
 		});
@@ -146,5 +146,67 @@ $(document).ready(function() {
 		last = $("#blog");
 		lastButton = $("#blog-nav");
 
+	});
+	$("#running-nav").click(function(){
+		$("#landing").hide(); // If a button is clicked, the landing text will go away
+		last.hide(); lastButton.removeClass("active");
+		lastButton.css("background-color", "");
+		$("#running-nav").toggleClass("active");
+		$("#running").fadeIn("slow");
+		last = $("#running");
+		lastButton = $("#running-nav");
+
+		// Displays CPI over the semesters
+		$(function () {
+		    $('#running-chart').highcharts({
+		        title: {
+		            text: 'Running'
+		        },
+		        subtitle: {
+		            text: ''
+		        },
+		        xAxis: {
+		        	title: {
+		        		text: 'Date'
+		        	},
+		            allowDecimals: false,
+		            // labels: {
+		            //     formatter: function () {
+		            //         return this.value; // clean, unformatted number for year
+		            //     }
+		            // }
+		            categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+		        },
+		        yAxis: {
+		        	min: 0,
+		        	max: 10,
+		            title: {
+		                text: 'Distance (km)'
+		            },
+		            allowDecimals: true,
+			        shared: true,
+		            useHTML: true
+		        },
+		        tooltip: {
+		            headerFormat: '{point.y} km - ', 
+		            pointFormat: '{point.x} {series.name}'
+		        },
+		        // Set the duration of animation
+		        plotOptions: {
+		            series: {
+		                animation: {
+		                    duration: 1500
+		                }
+		            }
+		        },
+		        series: [{
+		            name: 'January',
+		            data: [1.68, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.68, 0, 2.41, 1.43, 0, 0, 0, 2.36, 0, 3.2, 1.53, 2.06, 1.76, 3.51, 5.12, 4.02, 4.03, 3.63, 3.1, 0, 2.02]
+		        }, {
+		            name: 'February',
+		            data: [0, 0, 3.36, 3.05, 0, 2.03, 0, 0, 0, 5, 5.3, 5.07, 5, 5.23, 5.02, 5.21, 5.03, 5.02, 5.02, 6.75, 6.21]
+		        }]
+		    });
+		});
 	});
 });
